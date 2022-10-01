bafcdd99878932b2c6250ff7647456877030ccc2
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-10-01 22:35:24 +0100

    updated the ui

--- main.js
+++ main.js
@@ -125,6 +125,6 @@ function resetScore(){
 
 
 function openNav(){
-    navLinks.style.top = (navLinks.style.top === '47%') ? '-47%' : '47%';
+    navLinks.style.right = (navLinks.style.right === '50%') ? '-50%' : '50%';
     html.style.overflowY = (html.style.overflowY = "scroll") ? 'hidden' : 'scroll'
 }
